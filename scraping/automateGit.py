fa4dc79e8bb851c3b0f8dab31b2956220122961c
Will Jenson <will_jenson@outlook.com>
2021-03-19 08:20:00 -0500

    Added automation for git

--- scraping/automateGit.py
+++ scraping/automateGit.py
@@ -8,9 +8,12 @@ while True:
 
     if current_time == "07:30":
         pyautogui.typewrite("git add *")
+        pyautogui.press("enter")
         time.sleep(2)
         pyautogui.typewrite("git commit -m 'Updated CSV File'")
+        pyautogui.press("enter")
         time.sleep(2)
         pyautogui.typewrite("git push origin master")
+        pyautogui.press("enter")
         time.sleep(60)
         continue
